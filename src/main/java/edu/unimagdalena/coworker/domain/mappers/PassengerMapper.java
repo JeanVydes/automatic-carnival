cad3ff6eb66a6e2b55f36a173e1ad0d96f5b70a2
JeanVydes <videsrudas@proton.me>
2025-09-26 17:29:07 -0500

    add: passengerMapper

--- src/main/java/edu/unimagdalena/coworker/domain/mappers/PassengerMapper.java
+++ src/main/java/edu/unimagdalena/coworker/domain/mappers/PassengerMapper.java
@@ -1,5 +1,15 @@
 package edu.unimagdalena.coworker.domain.mappers;
 
-public class PassengerMapper {
-    
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import edu.unimagdalena.coworker.domain.dto.PassengerDTO;
+import edu.unimagdalena.coworker.domain.entities.Passenger;
+
+@Mapper(componentModel = "spring")
+public interface PassengerMapper {
+    PassengerDTO toDTO(Passenger passenger);
+
+    @Mapping(target = "id", ignore = true)
+    Passenger toEntity(PassengerDTO passengerDTO);
 }
